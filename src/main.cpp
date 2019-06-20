b03c6407465ff3ca0e502192b0db8435cf3ac926
Levi Rizki Saputra <levirs565@gmail.com>
2019-06-20 13:15:25 +0700

    using style from resources

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "window/mainwindow.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile style(":styles/base.css");
+    if (style.open(QIODevice::ReadOnly | QIODevice::Text))
+        a.setStyleSheet(style.readAll());
+
     MainWindow w;
     w.show();
 
