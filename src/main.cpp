3931b2a54552ae30693fc753d46fe373a738b6aa
Levi Rizki Saputra <levirs565@gmail.com>
2019-07-01 10:46:48 +0700

    add dark style

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,14 @@ int main(int argc, char *argv[])
     if (QFontDatabase::addApplicationFont(":fonts/MaterialIcons-Regular.ttf") == -1)
         qWarning() << "Cannot load material icons font";
 
+        QFile style(":styles/dark.css");
+        if (style.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            a.setStyleSheet(style.readAll());
+            style.close();
+        } else {
+            qWarning() << "Cannot load styles";
+        }
+
     MainWindow w(nullptr, conf);
     w.show();
 
